b68f05e5a7d7801a0cb3a38bda9ec611f7d2fe06
Tom Haas <tom97haas@icloud.com>
2018-09-30 18:44:27 -0300

    background and typescript update.

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//how to code time
 function GetClock(){
 var d=new Date();
 var nhour=d.getHours(),nmin=d.getMinutes();
@@ -9,42 +8,3 @@ document.getElementById('clockbox').innerHTML=clocktext;
 
 GetClock();
 setInterval(GetClock,1000);
-
-
-
-
-
-
-
-
-/*
-//variables
-var button = document.getElementById('button');
-var input = document.getElementById('input');
-var ol = document.querySelector("ol");
-
-//functions
-
-function inputLength(){
-  return input.value.length;
-}
-
-function addListItem(){
-  var li = document.createElement("li")
-  li.appendChild(document.createTextNode(input.value));
-  ol.appendChild(li);
-  input.value = "";
-}
-
-button.addEventListener("click", function(click){
-  if (inputLength() >0) {
-    addListItem();
-  }
-})
-
-input.addEventListener("keypress", function(enter){
-  if (inputLength() > 0 && enter.keyCode === 13) {
-    addListItem();
-    }
-})
-*/
